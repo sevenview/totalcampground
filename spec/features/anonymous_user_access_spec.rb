238fbcf9928205f1b6cdf165148491504e8dd69b
Steve Clarke <steve@sevenview.ca>
2014-06-27 16:57:52 -0230

    Add Log in / Log out, test authentication

---
+++ spec/features/anonymous_user_access_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Anonymous user' do
+  scenario 'gets login prompt on hitting the home page' do
+    visit root_path
+    expect(page).to have_content('You need to sign in or sign up before continuing')
+    expect(page).to have_content('Log In')
+    expect(current_path).to eq new_user_session_path
+  end
+
+  scenario 'doesn not see protected menu items' do
+    visit root_path
+    expect(page).to have_no_content('Log Out')
+    expect(page).to have_no_content('Reservation List')
+  end
+end
---
+++ spec/features/authenticated_user_access_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'Authenticated user' do
+  background do
+    sign_in FactoryGirl.create(:user)
+  end
+
+  scenario 'sees Home page after login' do
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content('Reservations')
+  end
+
+  scenario 'sees authenticated user menu items' do
+    expect(page).to have_content('Log Out')
+    expect(page).to have_content('Reservation List')
+  end
+end
