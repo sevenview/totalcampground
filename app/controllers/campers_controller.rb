556a1d67c4bd47e7ff8f059d6ca1f567b118f679
Steve Clarke <steve@sevenview.ca>
2014-06-29 10:27:17 -0230

    User can add a camper

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -2,4 +2,26 @@ class CampersController < ApplicationController
   def index
     @campers = Camper.all.order(:last_name, :first_name)
   end
+
+  def new
+    @camper = Camper.new
+  end
+
+  def create
+    @camper = Camper.new(camper_params)
+    if @camper.save
+      flash[:success] = "Created camper #{@camper.full_name}"
+      redirect_to campers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:first_name, :last_name, :address, :city,
+                                   :province, :postal_code, :country, :phone,
+                                   :mobile, :email, :notes)
+  end
 end
--- spec/features/camper_management_spec.rb
+++ spec/features/camper_management_spec.rb
@@ -13,7 +13,16 @@ feature 'Camper Management', type: :feature do
     expect(page).to have_css('tr', count: 3)
   end
 
-  scenario 'User can add a camper'
+  scenario 'User can add a camper' do
+    visit campers_path
+    click_link 'New Camper'
+    fill_in 'First name', with: 'John'
+    fill_in 'Last name', with: 'Doe'
+    fill_in 'Phone', with: '555-555-5555'
+    click_button 'Create Camper'
+    expect(page).to have_content 'Created camper John Doe'
+  end
+
   scenario 'User can delete a camper'
   scenario 'Deleting a camper does not affect any associated reservations'
   scenario 'User can update a camper'
