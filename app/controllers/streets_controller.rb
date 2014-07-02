7dd5c1fe2e1a091834aa9e10c0b90cd65b4de676
Steve Clarke <steve@sevenview.ca>
2014-07-02 14:39:08 -0230

    Add / update Street

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -2,4 +2,39 @@ class StreetsController < ApplicationController
   def index
     @streets = Street.all.order('name')
   end
+
+  def new
+    @street = Street.new
+  end
+
+  def create
+    @street = Street.new(street_params)
+    if @street.save
+      flash[:success] = 'Street created'
+      redirect_to streets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @street = Street.find(params[:id])
+  end
+
+  def update
+    @street = Street.find(params[:id])
+    @street.update_attributes(street_params)
+    if @street.save
+      flash[:success] = 'Street updated'
+      redirect_to streets_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def street_params
+    params.require(:street).permit(:name)
+  end
 end
--- spec/features/street_management_spec.rb
+++ spec/features/street_management_spec.rb
@@ -13,6 +13,37 @@ feature 'FEATURE: Street Management' do
     expect(page).to have_selector('td', text: street.name)
   end
 
-  scenario 'User can add a street'
-  scenario 'User can update a street'
+  scenario 'User can add a street' do
+    visit streets_path
+    click_link 'New Street'
+    fill_in 'Name', with: 'Main Street'
+    click_button 'Create Street'
+    expect(page).to have_content('Street created')
+  end
+
+  scenario 'User cannot add a street with invalid data' do
+    visit streets_path
+    click_link 'New Street'
+    fill_in 'Name', with: ''
+    click_button 'Create Street'
+    expect(page).to have_content('error')
+  end
+
+  scenario 'User can update a street' do
+    FactoryGirl.create(:street, name: 'Main Street')
+    visit streets_path
+    click_link 'Edit'
+    fill_in 'Name', with: 'Secondary Boulevard'
+    click_button 'Update Street'
+    expect(page).to have_content('Street updated')
+  end
+
+  scenario 'User cannot update a street with invalid data' do
+    FactoryGirl.create(:street, name: 'Main Street')
+    visit streets_path
+    click_link 'Edit'
+    fill_in 'Name', with: ''
+    click_button 'Update Street'
+    expect(page).to have_content('error')
+  end
 end
